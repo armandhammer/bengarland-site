3a5825a45d14d15a0a7443fab6f62e7d485bcde1
armandhammer <armandhammer@users.noreply.github.com>
2026-03-01 19:08:39 -0700

    test deploy

--- maps/livedlocations-code.js
+++ maps/livedlocations-code.js
@@ -94,6 +94,23 @@ d3.csv("stateslived.csv", function (data) {
 					return "#969696";
 				}
 			});
+			.on("mouseover", function (d) {
+
+				div.transition()
+					.duration(200)
+					.style("opacity", .9);
+
+				div.html(d.properties.name +
+					"<br/>Visited times: " + d.properties.visited)
+					.style("left", (d3.event.pageX) + "px")
+					.style("top", (d3.event.pageY - 28) + "px");
+			})
+			.on("mouseout", function (d) {
+
+				div.transition()
+					.duration(500)
+					.style("opacity", 0);
+			});
 
 
 		// Map the cities I have lived in!
@@ -121,7 +138,7 @@ d3.csv("stateslived.csv", function (data) {
 					div.transition()
 						.duration(200)
 						.style("opacity", .9);
-					div.text(d.place)
+					div.html(d.place + "<br/>Years lived: " + d.years)
 						.style("left", (d3.event.pageX) + "px")
 						.style("top", (d3.event.pageY - 28) + "px");
 				})
