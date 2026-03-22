f8e416be88caa422e3c11ab9a126e1d7383bd98c
armandhammer <armandhammer@users.noreply.github.com>
2026-03-22 16:37:21 -0600

    Update tutorial

--- tutorial/barchart-step1.js
+++ tutorial/barchart-step1.js
@@ -1,22 +1,9 @@
-// Chart size
 var width = 700;
 var height = 400;
 
-// Create the SVG area inside the div with id="chart"
 var svg = d3.select("#chart")
   .append("svg")
   .attr("width", width)
   .attr("height", height);
-
+  
 svg.style("border", "1px solid black");
-
-d3.csv("fruit.csv").then(function(data) {
-
-  // Convert value to number
-  data.forEach(function(d) {
-    d.value = +d.value;
-  });
-
-  console.log(data);
-
-});
