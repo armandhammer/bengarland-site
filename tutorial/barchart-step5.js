467b6dee7be149431248ad3842754c4727ceb557
armandhammer <armandhammer@users.noreply.github.com>
2026-03-22 16:51:19 -0600

    Update tutorial

---
+++ tutorial/barchart-step5.js
@@ -0,0 +1,69 @@
+// Chart size
+var width = 700;
+var height = 400;
+
+// Margins
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 60,
+  left: 60
+};
+
+var innerWidth = width - margin.left - margin.right;
+var innerHeight = height - margin.top - margin.bottom;
+
+// Create SVG
+var svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// Optional border so we can see the SVG area
+svg.style("border", "1px solid black");
+
+// Create chart group
+var chartGroup = svg.append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Load data
+d3.csv("fruit.csv").then(function (data) {
+
+  // Convert value to number
+  data.forEach(function (d) {
+    d.value = +d.value;
+  });
+
+  // X scale
+  var xScale = d3.scaleBand()
+    .domain(data.map(function (d) { return d.category; }))
+    .range([0, innerWidth])
+    .padding(0.2);
+
+  // Y scale
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, function (d) { return d.value; })])
+    .range([innerHeight, 0]);
+
+  // Add x-axis
+  chartGroup.append("g")
+    .attr("transform", "translate(0," + innerHeight + ")")
+    .call(d3.axisBottom(xScale));
+
+  // Add y-axis
+  chartGroup.append("g")
+    .call(d3.axisLeft(yScale));
+
+  // Draw bars
+  chartGroup.selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", function (d) { return xScale(d.category); })
+    .attr("y", function (d) { return yScale(d.value); })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function (d) { return innerHeight - yScale(d.value); })
+    .attr("fill", "steelblue");
+
+});
